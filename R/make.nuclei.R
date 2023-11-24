d3bc872b0bd96727d1dc539377b67e36580cb95e
geocaruso <geoffrey.caruso@gmail.com>
2023-11-24 11:58:22 +0100

    corrected make.nuclei so population is recomputed after within holes are filled in

--- R/make.nuclei.R
+++ R/make.nuclei.R
@@ -37,8 +37,8 @@
 #'
 #' @examples
 make.nuclei<-function(r, urban_th=6, pop_th=150, neighb=4,
-                      cl_brks=c(150,500,1000,2500,5000,10000,50000,200000,sum(terra::values(r),na.rm=TRUE)),
-                      sfplot=TRUE
+                      cl_brks=c(150,500,1000,2500,5000,10000,25000,50000,200000,sum(terra::values(r),na.rm=TRUE)),
+                      sfplot=TRUE, fillHoles=TRUE
 ){
   TOTPOP<-sum(terra::values(r),na.rm=TRUE)
   message(paste("Total population in raster:",TOTPOP))
@@ -53,48 +53,52 @@ make.nuclei<-function(r, urban_th=6, pop_th=150, neighb=4,
   message(paste("Number of patches:",length(unique(freq_rp$value))))
   
   #rp is any patch, and many are of size 1 cell.
-  #Following Belgian case in 2001 a noyau must be at least 150 inhabi
+  #Following Belgian case in 2001 a noyau must be at least 150 inhab
   #We thus zonal stats original pop raster r to rp then limit rp's by pop_th>=150
   zonal_sum<-terra::zonal(r,rp, fun="sum")
-  message(paste("Population within cells below the urban threshold:",TOTPOP-sum(zonal_sum[,2])))
+  message(paste("Population within cells below the urban threshold:",
+                TOTPOP-sum(zonal_sum[,2])))
   #population considered dispersed whatever the definition of nuclei
   # before applying pop threshold to nuclei
   nucleus_id<-zonal_sum$patches[zonal_sum[,2]>=pop_th]
   message(paste("Number of population clusters (Noyaux)",length(nucleus_id)))
-  
   rnuclei <- terra::mask(rp, rp, nucleus_id, inverse=TRUE) #this set NA's in the raster of patches where patches do not reach pop limit
   
-  #convert raster to polygons 
+  #convert raster to spatVector 
   vnuclei0<-terra::as.polygons(rnuclei) #spatvector
+
+  #get rid of isolated cells within a polygon, i.e. include them in noyaux
+  if (fillHoles==TRUE) {
+    vnuclei<-terra::fillHoles(vnuclei0, inverse=FALSE)
+  } else {
+    vnuclei<-vnuclei0
+  }
   
-  #get rid of isolated cells within a polygon.
-  vnuclei<-terra::fillHoles(vnuclei0, inverse=FALSE)
-  #terra::plot(vnuclei)
-  
-  #Change to sf
+  #Create sf (thus df)
   nuclei_sf<-sf::st_as_sf(vnuclei)
   
-  #Add attributes to nuclei sf
-  nuclei_sf_pop0<-merge(nuclei_sf, freq_rp[,2:3], all.x= TRUE, by.x="patches",by.y="value")
-  nuclei_sf_pop<-merge(nuclei_sf_pop0, zonal_sum, all.x= TRUE, by.x="patches",by.y="patches")
-  
-  names(nuclei_sf_pop)[1:3]<-c("nucleus_id","cellcount","population")
-  nuclei_sf_pop$km2_holes<-nuclei_sf_pop$cellcount*200^2*10^(-6)
-  nuclei_sf_pop$km2<-sf::st_area(nuclei_sf_pop)*10^(-6)
-  nuclei_sf_pop$density_km2<-nuclei_sf_pop$population/nuclei_sf_pop$km2
+  #recompute pop of noyaux after filling the holes, i.e. reintegrate those cells
+  # excluded by urban_th (see r2 from r) but being within filled hole of a noyaux
+  nuclei_sf$population<-terra::zonal(r,vnuclei, fun="sum", na.rm=TRUE)[,1]
+  # Also recompute number of cells per noyaux (i.e surfaces)
+  #  I use sum of those being true >1 since count is not a zonal function
+  nuclei_sf$cellcount<-terra::zonal(r>=1,vnuclei, fun="sum", na.rm=TRUE)[,1] 
+  # Add surface and density
+  nuclei_sf$km2<-sf::st_area(nuclei_sf)*10^(-6)
+  nuclei_sf$density_km2<-nuclei_sf$population/nuclei_sf$km2
   
   #Assemble nuclei summary table
-  cl.int.nuclei<-classInt::classIntervals(nuclei_sf_pop$population,
+  cl.int.nuclei<-classInt::classIntervals(nuclei_sf$population,
                                           style = "fixed",
                                           fixedBreaks=cl_brks)
-  nuclei_sf_pop$cl.nucleus<-classInt::findCols(cl.int.nuclei, factor=TRUE)
+  nuclei_sf$cl.nucleus<-classInt::findCols(cl.int.nuclei, factor=TRUE)
   
-  group_n <- aggregate(nuclei_sf_pop$population, list(nuclei_sf_pop$cl.nucleus), length)
-  group_sum <- aggregate(nuclei_sf_pop$population, list(nuclei_sf_pop$cl.nucleus), sum)
+  group_n <- aggregate(nuclei_sf$population, list(nuclei_sf$cl.nucleus), length)
+  group_sum <- aggregate(nuclei_sf$population, list(nuclei_sf$cl.nucleus), sum)
   
   nuclei_table0<-cbind(group_n,group_sum[,2])
   levels(nuclei_table0$Group.1)<-c(levels(nuclei_table0$Group.1),"Dispersed")
-  nuclei_table<-rbind(c("Dispersed",NA,TOTPOP-sum(nuclei_sf_pop$population)),nuclei_table0)
+  nuclei_table<-rbind(c("Dispersed",NA,TOTPOP-sum(nuclei_sf$population)),nuclei_table0)
   
   names(nuclei_table)<-c("cl.nuclei","n","pop")
   nuclei_table$pop<-as.numeric(nuclei_table$pop)
@@ -105,8 +109,8 @@ make.nuclei<-function(r, urban_th=6, pop_th=150, neighb=4,
   nuclei_table$cumul.pc.pop<-cumsum(nuclei_table$pc.pop)
   
   #Map
-  if(sfplot==TRUE){plot(nuclei_sf_pop)}
+  if(sfplot==TRUE){plot(nuclei_sf)}
   
-  return(list(sf=nuclei_sf_pop, classes=nuclei_table))
+  return(list(sf=nuclei_sf, classes=nuclei_table))
   
 }
